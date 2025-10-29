1fb14ba2668202beb02f9a802e39396bfd3b9ccd
Rhakshitha-S <166644440+Rhakshitha-S@users.noreply.github.com>
2025-10-29 21:28:16 +0530

    only .com domain allowed
    Updated regex pattern to support multiple domain extensions.

--- email_validator.py
+++ email_validator.py
@@ -2,8 +2,8 @@ import re
 
 def is_valid_email(email):
         # Improved regex supporting multiple domain extensions
-    pattern = r'^[\w\.-]+@[\w\.-]+\.(com|org|net|edu)$'
+    #pattern = r'^[\w\.-]+@[\w\.-]+\.(com|org|net|edu)$'
         #only accepts .com extension (fails to acknowledge other domains) 
-    #pattern = r'^[\w\.-]+@[\w\.-]+\.com$'
+    pattern = r'^[\w\.-]+@[\w\.-]+\.com$'
     #pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
     return re.match(pattern, email) is not None
